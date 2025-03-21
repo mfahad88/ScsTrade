2c27057d84c6bcdce73b726c2538632fba7a81b3
Fahad <mfahad88@outlook.com>
2025-03-21 13:57:24 +0500

    AnnouncementsFragment partial done

--- app/src/main/java/com/example/scstrade/views/search/SearchActivity.kt
+++ app/src/main/java/com/example/scstrade/views/search/SearchActivity.kt
@@ -6,13 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.scstrade.R
+import com.example.scstrade.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
+    lateinit var binding:ActivitySearchBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_search)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
