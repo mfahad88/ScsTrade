d836d5080111f193db312100c1d1f660e991ff37
Fahad <mfahad88@outlook.com>
2025-05-07 19:32:05 +0500

    Portfolio inprogress

---
+++ app/src/main/java/com/example/scstrade/views/aof/fragments/accountopening/AccountOpeningTwoFragment.kt
@@ -0,0 +1,26 @@
+package com.example.scstrade.views.aof.fragments.accountopening
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.scstrade.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [AccountOpeningTwoFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class AccountOpeningTwoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account_opening_two, container, false)
+    }
+
+
+}
