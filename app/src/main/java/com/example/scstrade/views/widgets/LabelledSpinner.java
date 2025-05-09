4a21ccf3d55120d562d0ac8bf17e463378243ed9
Administrator <mfahad88@outlook.com>
2025-05-10 00:53:30 +0500

    From home

---
+++ app/src/main/java/com/example/scstrade/views/widgets/LabelledSpinner.java
@@ -0,0 +1,50 @@
+package com.example.scstrade.views.widgets;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.scstrade.R;
+import com.example.scstrade.databinding.LabelledSpinnerBinding;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class LabelledSpinner extends TextInputLayout {
+    LabelledSpinnerBinding binding;
+    public LabelledSpinner(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context,attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        binding = LabelledSpinnerBinding.inflate(LayoutInflater.from(context),this,true);
+        if(attrs!=null){
+            TypedArray a=context.getTheme().obtainStyledAttributes(
+                    attrs,
+                    R.styleable.LabelledSpinner,
+                    0,0
+            );
+            try {
+                String hint = a.getString(R.styleable.LabelledSpinner_hintLabel);
+                String dropdownLabel = a.getString(R.styleable.LabelledSpinner_dropdownLabel);
+
+                if(hint!=null){
+                    binding.textInput.setHint(hint);
+                }
+                if(dropdownLabel!=null){
+                    binding.dropdown.setHint(dropdownLabel);
+                }
+
+
+                binding.dropdown.setAdapter(new ArrayAdapter(context, android.R.layout.simple_list_item_1,a.getTextArray(R.styleable.LabelledSpinner_entries)));
+
+            }finally {
+                a.recycle();
+            }
+        }
+    }
+}
